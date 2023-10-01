a5c507c7cf36f3620653dcccc6c5c1a1e3ce94be
Maraj Hossain <marajhossaincse@gmail.com>
2023-10-01 19:08:28 +0600

    Initial commit.

---
+++ CupcakeCorner/CupcakeCornerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CupcakeCornerApp.swift
+//  CupcakeCorner
+//
+//  Created by Maraj Hossain on 10/1/23.
+//
+
+import SwiftUI
+
+@main
+struct CupcakeCornerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
