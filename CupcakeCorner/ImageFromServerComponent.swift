a70a6a1a37d77c493dcff7985e1cac9d7e7d12ab
Maraj Hossain <marajhossaincse@gmail.com>
2023-10-01 21:51:37 +0600

    R&D on loading an image from remote server.

---
+++ CupcakeCorner/ImageFromServerComponent.swift
@@ -0,0 +1,31 @@
+//
+//  ImageFromServerComponent.swift
+//  CupcakeCorner
+//
+//  Created by Maraj Hossain on 10/1/23.
+//
+
+import SwiftUI
+
+struct ImageFromServerComponent: View {
+    var body: some View {
+        AsyncImage(url: URL(string: "https://hws.dev/img/bad.png")) { phase in
+            if let image = phase.image {
+                image
+                    .resizable()
+                    .scaledToFit()
+            } else if phase.error != nil {
+                Text("There was an error loading the image.")
+            } else {
+                ProgressView()
+            }
+        }
+        .frame(width: 200, height: 200)
+    }
+}
+
+struct ImageFromServerComponent_Previews: PreviewProvider {
+    static var previews: some View {
+        ImageFromServerComponent()
+    }
+}
