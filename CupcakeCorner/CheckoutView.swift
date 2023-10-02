8a13099adf7641a2aeba6b4ababd426485766492
Maraj Hossain <marajhossaincse@gmail.com>
2023-10-02 13:24:59 +0600

    Add checkout view.

--- CupcakeCorner/CheckoutView.swift
+++ CupcakeCorner/CheckoutView.swift
@@ -10,7 +10,30 @@ import SwiftUI
 struct CheckoutView: View {
     @ObservedObject var order: Order
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ScrollView {
+            VStack {
+                AsyncImage(
+                    url: URL(string: "http://hws.dev/img/cupcakes@3x.jpg"),
+                    scale: 3)
+                { image in
+                    image
+                        .resizable()
+                        .scaledToFit()
+
+                } placeholder: {
+                    ProgressView()
+                }
+                .frame(height: 233)
+
+                Text("Your total in \(order.cost, format: .currency(code: "USD"))")
+                    .font(.title)
+
+                Button("Place order", action: {})
+                    .padding()
+            }
+        }
+        .navigationTitle("Check out")
+        .navigationBarTitleDisplayMode(.inline)
     }
 }
 
